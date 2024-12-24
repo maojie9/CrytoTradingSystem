e475318c317befc4318b0aa5774efef5c5549bf8
Mao Jie <maojie9@gmail.com>
2024-12-25 03:20:07 +0800

    Remove the implementation into database as both models are api data transfer to Coin model only

--- src/main/java/com/maojie/trading/model/BinanceCoin.java
+++ src/main/java/com/maojie/trading/model/BinanceCoin.java
@@ -1,16 +1,11 @@
 package com.maojie.trading.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity
 public class BinanceCoin {
 
-    @Id
     @JsonProperty("symbol")
     private String symbol;
 
--- src/main/java/com/maojie/trading/model/HuobiCoin.java
+++ src/main/java/com/maojie/trading/model/HuobiCoin.java
@@ -1,16 +1,11 @@
 package com.maojie.trading.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity
 public class HuobiCoin {
 
-    @Id
     @JsonProperty("symbol")
     private String symbol;
 
