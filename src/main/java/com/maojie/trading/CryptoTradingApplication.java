982c6e0f8ebdb7e4651a790cd8b5a723f765c525
Mao Jie <maojie9@gmail.com>
2024-12-24 20:55:32 +0800

    Create a 10 seconds interval scheduler to retrieve the pricing from the source above and store the best pricing into the database.

--- src/main/java/com/maojie/trading/CryptoTradingApplication.java
+++ src/main/java/com/maojie/trading/CryptoTradingApplication.java
@@ -1,13 +1,35 @@
 package com.maojie.trading;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maojie.trading.service.CoinService;
 
 @SpringBootApplication
+@EnableScheduling
+@RestController
 public class CryptoTradingApplication {
+
+	@Autowired
+    CoinService coinService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoTradingApplication.class, args);
 	}
+
+	@Scheduled(fixedRate = 10000)
+    public void greeting() {
+		try {
+			coinService.updateLatestCoinPriceFromSource();
+			System.out.println("Coins Updated");
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+    }
 
 }
